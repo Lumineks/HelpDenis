50d8e3f0e8cae4fdb2200fb0206087f87ed9efae
Aragorn <mihasik323@gmail.com>
2020-05-07 12:01:40 +0300

    Add project files.

---
+++ HelpDenis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelpDenis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
